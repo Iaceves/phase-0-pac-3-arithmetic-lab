85963418bb74c3024346eb4718eb5b4e203e55de
iaceves <acevesaceves97@gmail.com>
2022-07-27 18:58:30 -0400

    commit tree 2bbfb187e5ab0dcec437809cc82bbff6208b7cbd

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+
+}
+
+function multiply() {
+    
+}
+
+function subtract() {
+    
+}
+
+function divide() {
+    
+}
+
+function increment(n) {
+    return (n + 1);
+}
+
+function decrement(n) {
+    return (n - 1);
+}
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function makeInt(n){
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+    return parseFloat(n, 10)
+}
